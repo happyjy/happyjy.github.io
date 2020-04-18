97ee52ce6216402f8a2893fb773b9446984dc12a
happyjy <okwoyjy@gmail.com>
2020-04-18 16:34:03 +0900

    #12 - Tags PageCreate a GatsbyJS Blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,7 @@
-const { slugify } = require(`./src/util/utilityFunctions`);
+const { slugify } = require('./src/util/utilityFunctions');
 const authors = require('./src/util/authors');
 const path = require('path');
+const _ = require('lodash');
 
 exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions;
@@ -16,7 +17,11 @@ exports.onCreateNode = ({ node, actions }) => {
 
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions;
-  const singlePostTemplate = path.resolve('src/templates/single-post.js');
+
+  const templates = {
+    singlePost: path.resolve('src/templates/single-post.js'),
+    tagsPage: path.resolve('src/templates/tags-page.js'),
+  };
 
   return graphql(`
     {
@@ -24,7 +29,9 @@ exports.createPages = ({ actions, graphql }) => {
         edges {
           node {
             frontmatter {
-              author
+              author,
+              tags, 
+              category
             }
             fields {
               slug
@@ -40,11 +47,11 @@ exports.createPages = ({ actions, graphql }) => {
     
     const posts = res.data.allMarkdownRemark.edges;
     console.log({posts});
-
+    // Create single blog post pages
     posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
-        component: singlePostTemplate,
+        component: templates.singlePost,
         context: {
           // Passing slug for template to use to get post
           slug: node.fields.slug,
@@ -53,6 +60,48 @@ exports.createPages = ({ actions, graphql }) => {
         },
       });
     });
+
+    //Get allTags
+    let tags = [];
+    let category = [];
+    _.each(posts, edge => {
+      if (_.get(edge, 'node.frontmatter.tags')) {
+        tags = tags.concat(edge.node.frontmatter.tags)
+      }
+    })
+
+    posts.forEach(({ node }) => {
+      console.log("# category ");
+      console.log(node);
+      category = category.concat(node.frontmatter.category);
+      return category;
+    })
+
+    //[javascript, react, ...]
+    //{javascript: 5, react: 3, ...}
+    let tagPostCounts = {};
+    tags.forEach(tag => {
+      tagPostCounts[tag] = (tagPostCounts[tag] || 0) + 1;
+    });
+
+    tags = _.uniq(tags);
+    // tags = [...new Set(tags)];
+
+    console.log("#########################")
+    console.log({tags, tagPostCounts});
+    console.log({category});
+
+
+    // Create tags page 
+    createPage({
+      path: '/tags',
+      component: templates.tagsPage,
+      context: {
+        tags,
+        tagPostCounts
+      }
+    });
+
   });
 }
 
