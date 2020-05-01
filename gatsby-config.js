50240607c23154ec91a08f4a1b2e53454f4755b6
happyjy <okwoyjy@gmail.com>
2020-05-01 10:38:23 +0900

    gatsby-config 설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-const _ = require('lodash');
+const _ = require("lodash")
 
 module.exports = {
   siteMetadata: {
@@ -12,14 +12,21 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
-        path: `${__dirname}/src/_posts`,
+        path: `${__dirname}/_posts`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/_images`,
+        path: `${__dirname}/_posts/_images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
     {
@@ -50,47 +57,48 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         tableOfContents: {
-          maxDepth: 4,
+          maxDepth: 3,
         },
         plugins: [
           {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
-              wrapperStyle: fluidResult => `flex:${_.round(fluidResult.aspectRatio, 2)};`,
+              wrapperStyle: fluidResult =>
+                `flex:${_.round(fluidResult.aspectRatio, 2)};`,
             },
           },
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
-              classPrefix: 'language-',
+              classPrefix: "language-",
               inlineCodeMarker: null,
               aliases: {},
               showLineNumbers: true,
             },
           },
           {
-            resolve: 'gatsby-remark-emojis',
+            resolve: "gatsby-remark-emojis",
             options: {
               active: true,
-              class: 'emoji-icon',
+              class: "emoji-icon",
               size: 64,
               styles: {
-                display: 'inline',
-                margin: '0',
-                'margin-top': '1px',
-                position: 'relative',
-                top: '5px',
-                width: '25px',
+                display: "inline",
+                margin: "0",
+                "margin-top": "1px",
+                position: "relative",
+                top: "5px",
+                width: "25px",
               },
             },
           },
           `gatsby-remark-autolink-headers`,
           `gatsby-remark-katex`,
           {
-            resolve: 'gatsby-remark-external-links',
+            resolve: "gatsby-remark-external-links",
             options: {
-              target: '_blank',
+              target: "_blank",
             },
           },
         ],
