bc9400c5343b42ce8e1bbaccf598259a6f9144f1
happyjy <okwoyjy@gmail.com>
2020-05-16 17:59:57 +0900

    gatsby 설정 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,111 +1,110 @@
-const _ = require("lodash")
+const _ = require('lodash');
 
 module.exports = {
-  siteMetadata: {
-    title: `jyoon blog version2`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/_posts`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/_posts/_images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-catch-links`,
-    // `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        tableOfContents: {
-          maxDepth: 3,
-        },
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-              wrapperStyle: fluidResult =>
-                `flex:${_.round(fluidResult.aspectRatio, 2)};`,
-            },
-          },
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              classPrefix: "language-",
-              inlineCodeMarker: null,
-              aliases: {},
-              showLineNumbers: true,
-            },
-          },
-          {
-            resolve: "gatsby-remark-emojis",
-            options: {
-              active: true,
-              class: "emoji-icon",
-              size: 64,
-              styles: {
-                display: "inline",
-                margin: "0",
-                "margin-top": "1px",
-                position: "relative",
-                top: "5px",
-                width: "25px",
-              },
-            },
-          },
-          `gatsby-remark-autolink-headers`,
-          `gatsby-remark-katex`,
-          {
-            resolve: "gatsby-remark-external-links",
-            options: {
-              target: "_blank",
-            },
-          },
-        ],
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+	siteMetadata: {
+		title: `jyoon blog version2`,
+		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+		author: `@gatsbyjs`
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `markdown-pages`,
+				path: `${__dirname}/_posts`
+			}
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/_posts/_images`
+			}
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`
+			}
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `pages`,
+				path: `${__dirname}/src/pages`
+			}
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `gatsby-starter-default`,
+				short_name: `starter`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/icon.png` // This path is relative to the root of the site.
+			}
+		},
+		`gatsby-plugin-sass`,
+		`gatsby-plugin-catch-links`,
+		// `gatsby-transformer-remark`,
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				tableOfContents: {
+					maxDepth: 3
+				},
+				plugins: [
+					{
+						resolve: `gatsby-remark-images`,
+						options: {
+							maxWidth: 590,
+							wrapperStyle: (fluidResult) => `flex:${_.round(fluidResult.aspectRatio, 2)};`
+						}
+					},
+					{
+						resolve: `gatsby-remark-prismjs`,
+						options: {
+							classPrefix: 'language-',
+							inlineCodeMarker: null,
+							aliases: {},
+							showLineNumbers: false
+						}
+					},
+					{
+						resolve: 'gatsby-remark-emojis',
+						options: {
+							active: true,
+							class: 'emoji-icon',
+							size: 64,
+							styles: {
+								display: 'inline',
+								margin: '0',
+								'margin-top': '1px',
+								position: 'relative',
+								top: '5px',
+								width: '25px'
+							}
+						}
+					},
+					`gatsby-remark-autolink-headers`,
+					`gatsby-remark-katex`,
+					{
+						resolve: 'gatsby-remark-external-links',
+						options: {
+							target: '_blank'
+						}
+					}
+				]
+			}
+		}
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+	]
+};
