65a06ff9f37d1934fd169561d13adba3667d1eae
happyjy <okwoyjy@gmail.com>
2020-05-29 21:45:25 +0900

    [blog개선] - pagenation 제거/read more 버튼 제거

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,67 +1,62 @@
-import React from "react"
-import { Link } from "gatsby"
-import {
-  Badge,
-  Card,
-  CardTitle,
-  CardText,
-  CardSubtitle,
-  CardBody,
-} from "reactstrap"
+import React from 'react';
+import { Link } from 'gatsby';
+import { Badge, Card, CardTitle, CardText, CardSubtitle, CardBody } from 'reactstrap';
 // import Img from "gatsby-image"
-import { slugify } from "../util/utilityFunctions"
+import { slugify } from '../util/utilityFunctions';
 
 const Post = ({ title, author, slug, date, body, fluid, category, tags }) => {
-  return (
-    <Card>
-      {/* {!fluid ? (
+	return (
+		<Card>
+			{/* {!fluid ? (
         ""
       ) : (
         <Link to={slug}>
           <Img className="card-image-top cardImg" fluid={fluid} />
         </Link>
       )} */}
-      <CardBody>
-        <CardTitle>
-          <Link to={slug}>
-            <h2>{title}</h2>
-          </Link>
-        </CardTitle>
-        <CardSubtitle>
-          <span className="text-info">{date}</span>
-          {author ? `  by` : ""} <span className="text-info">{author}</span>
-        </CardSubtitle>
-        <ul className="post-category">
-          <li>
-            <Link to={`/category/${category}`}>
-              <Badge color="danger" className="text-uppercase" pill>
-                {category}
-              </Badge>
-            </Link>
-          </li>
-        </ul>
-        <ul className="post-tags">
-          {tags &&
-            tags.length > 0 &&
-            tags.map(tag => (
-              <li key={tag}>
-                <Link to={`/tag/${slugify(tag)}`}>
-                  <Badge color="primary" className="text-uppercase" pill>
-                    {tag}
-                  </Badge>
-                </Link>
-              </li>
-            ))}
-        </ul>
-        <CardText>
-          <Link to={slug}>{body}</Link>
-        </CardText>
-        <Link to={slug} className="btn btn-outline-primary float-right">
+			<CardBody>
+				<CardTitle>
+					<Link to={slug}>
+						<h2>{title}</h2>
+					</Link>
+				</CardTitle>
+				<CardSubtitle>
+					<span className='text-info'>{date}</span>
+					{author ? `  by` : ''} <span className='text-info'>{author}</span>
+				</CardSubtitle>
+				<div className='post-type-container'>
+					<ul className='post-category'>
+						<li>
+							<Link to={`/category/${category}`}>
+								<Badge color='danger' className='text-uppercase' pill>
+									{category}
+								</Badge>
+							</Link>
+						</li>
+					</ul>
+					<ul className='post-tags'>
+						{tags &&
+							tags.length > 0 &&
+							tags.map((tag) => (
+								<li key={tag}>
+									<Link to={`/tag/${slugify(tag)}`}>
+										<Badge color='primary' className='text-uppercase' pill>
+											{tag}
+										</Badge>
+									</Link>
+								</li>
+							))}
+					</ul>
+				</div>
+				<CardText>
+					<Link to={slug}>{body}</Link>
+				</CardText>
+				{/* <Link to={slug} className="btn btn-outline-primary float-right">
           Read more
-        </Link>
-      </CardBody>
-    </Card>
-  )
-}
+        </Link> */}
+			</CardBody>
+		</Card>
+	);
+};
 
-export default Post
+export default Post;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,80 +1,73 @@
-import React from "react"
-import Layout from "../components/Layout"
-import { graphql, StaticQuery } from "gatsby"
-import SEO from "../components/seo"
-import Post from "../components/Post"
-import PaginationLinks from "../components/PaginationLink"
+import React from 'react';
+import Layout from '../components/Layout';
+import { graphql, StaticQuery } from 'gatsby';
+import SEO from '../components/seo';
+import Post from '../components/Post';
+import PaginationLinks from '../components/PaginationLink';
 
 const IndexPage = () => {
-  const postsPerPage = 5
-  let numberOfPages
-  return (
-    <Layout pageTitle="developBlog">
-      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <StaticQuery
-        query={indexQuery}
-        render={data => {
-          numberOfPages = Math.ceil(
-            data.allMarkdownRemark.totalCount / postsPerPage
-          )
-          return (
-            <div>
-              {data.allMarkdownRemark.edges.map(({ node }) => (
-                <Post
-                  key={node.id}
-                  slug={node.fields.slug}
-                  title={node.frontmatter.title}
-                  author={node.frontmatter.author}
-                  date={node.frontmatter.date}
-                  body={node.excerpt}
-                  category={node.frontmatter.category}
-                  tags={node.frontmatter.tags}
-                  fluid={
-                    node.frontmatter.image &&
-                    node.frontmatter.image.childImageSharp.fluid
-                  }
-                />
-              ))}
-              <PaginationLinks currentPage={1} numberOfPages={numberOfPages} />
-            </div>
-          )
-        }}
-      />
-    </Layout>
-  )
-}
+	const postsPerPage = 5;
+	let numberOfPages;
+	return (
+		<Layout pageTitle='developBlog'>
+			<SEO title='Home' keywords={[ `gatsby`, `application`, `react` ]} />
+			<StaticQuery
+				query={indexQuery}
+				render={(data) => {
+					numberOfPages = Math.ceil(data.allMarkdownRemark.totalCount / postsPerPage);
+					return (
+						<div>
+							{data.allMarkdownRemark.edges.map(({ node }) => (
+								<Post
+									key={node.id}
+									slug={node.fields.slug}
+									title={node.frontmatter.title}
+									author={node.frontmatter.author}
+									date={node.frontmatter.date}
+									body={node.excerpt}
+									category={node.frontmatter.category}
+									tags={node.frontmatter.tags}
+									fluid={node.frontmatter.image && node.frontmatter.image.childImageSharp.fluid}
+								/>
+							))}
+							{/* <PaginationLinks currentPage={1} numberOfPages={numberOfPages} /> */}
+						</div>
+					);
+				}}
+			/>
+		</Layout>
+	);
+};
 
 const indexQuery = graphql`
-  query indexQuery {
-    allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC }
-      limit: 5
-    ) {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "MMM Do YYYY")
-            author
-            tags
-            category
-            image {
-              childImageSharp {
-                fluid(maxWidth: 600) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-          fields {
-            slug
-          }
-          excerpt
-        }
-      }
-    }
-  }
-`
-export default IndexPage
+	query indexQuery {
+		# allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: 5) {
+		allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+			totalCount
+			edges {
+				node {
+					id
+					frontmatter {
+						title
+						date(formatString: "MMM Do YYYY")
+						author
+						tags
+						category
+						image {
+							childImageSharp {
+								fluid(maxWidth: 600) {
+									...GatsbyImageSharpFluid
+								}
+							}
+						}
+					}
+					fields {
+						slug
+					}
+					excerpt
+				}
+			}
+		}
+	}
+`;
+export default IndexPage;
