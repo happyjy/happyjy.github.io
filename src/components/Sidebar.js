0f0c8520e6cdada1d6ceaac165de8d430fc81eed
happyjy <okwoyjy@gmail.com>
2020-06-01 00:08:32 +0900

    blog 파일 beautify 적용

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,64 +1,52 @@
-import React from "react"
+import React from 'react';
 import {
-  Card,
-  CardBody,
-  CardTitle,
-  // Form,
-  // FormGroup,
-  // Input,
-  CardText,
-} from "reactstrap"
-import Img from "gatsby-image"
-import { graphql, StaticQuery, Link } from "gatsby"
+	Card,
+	CardBody,
+	CardTitle,
+	// Form,
+	// FormGroup,
+	// Input,
+	CardText
+} from 'reactstrap';
+import Img from 'gatsby-image';
+import { graphql, StaticQuery, Link } from 'gatsby';
 
 const Sidebar = ({ author, authorFluid, tableOfContents }) => (
-  <div>
-    {author && (
-      <Card>
-        <Img className="card-image-top" fluid={authorFluid} />
-        <CardBody>
-          <CardTitle className="text-center text-uppercase mb-3">
-            {author.name}
-          </CardTitle>
-          <CardText>{author.bio}</CardText>
-          <div className="author-social-links text-center author">
-            <ul>
-              <li>
-                <a
-                  href={author.github}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="github"
-                >
-                  <i className="fab fa-github-square fa-2x"></i>
-                </a>
-              </li>
-              <li>
-                <a
-                  href={author.instagram}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="instagram"
-                >
-                  <i className="fab fa-instagram-square fa-2x"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </CardBody>
-      </Card>
-    )}
-    {tableOfContents && (
-      <Card>
-        <CardBody>
-          <CardTitle className="text-center text-uppercase mb-3">
-            TABLE OF CONTENTS
-          </CardTitle>
-          <div
-            className="toc"
-            dangerouslySetInnerHTML={{ __html: tableOfContents }}
-          />
-          {/* <Form className="text-center">
+	<div>
+		{author && (
+			<Card>
+				<Img className='card-image-top' fluid={authorFluid} />
+				<CardBody>
+					<CardTitle className='text-center text-uppercase mb-3'>{author.name}</CardTitle>
+					<CardText>{author.bio}</CardText>
+					<div className='author-social-links text-center author'>
+						<ul>
+							<li>
+								<a href={author.github} target='_blank' rel='noopener noreferrer' className='github'>
+									<i className='fab fa-github-square fa-2x' />
+								</a>
+							</li>
+							<li>
+								<a
+									href={author.instagram}
+									target='_blank'
+									rel='noopener noreferrer'
+									className='instagram'
+								>
+									<i className='fab fa-instagram-square fa-2x' />
+								</a>
+							</li>
+						</ul>
+					</div>
+				</CardBody>
+			</Card>
+		)}
+		{tableOfContents && (
+			<Card>
+				<CardBody>
+					<CardTitle className='text-center text-uppercase mb-3'>TABLE OF CONTENTS</CardTitle>
+					<div className='toc' dangerouslySetInnerHTML={{ __html: tableOfContents }} />
+					{/* <Form className="text-center">
           <FormGroup>
             <Input 
               type="email" 
@@ -69,83 +57,72 @@ const Sidebar = ({ author, authorFluid, tableOfContents }) => (
             Subscribe
           </button>
         </Form> */}
-        </CardBody>
-      </Card>
-    )}
-    <Card>
-      <CardBody>
-        <CardTitle>Recet Posts</CardTitle>
-        <StaticQuery
-          query={sidebarQuery}
-          render={data => (
-            <div>
-              {data.allMarkdownRemark.edges.shift() &&
-                data.allMarkdownRemark.edges.shift() &&
-                data.allMarkdownRemark.edges.map(({ node }) => (
-                  <Card key={node.id}>
-                    {!node.frontmatter.image ? (
-                      ""
-                    ) : (
-                      <Link to={node.fields.slug}>
-                        <Img
-                          className="card-image-top"
-                          fluid={node.frontmatter.image.childImageSharp.fluid}
-                        />
-                      </Link>
-                    )}
-                    {/* <CardBody> */}
-                    {/* <CardTitle> */}
-                    <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
-                    {/* </CardTitle> */}
-                    {/* </CardBody> */}
-                  </Card>
-                ))}
-            </div>
-          )}
-        />
-      </CardBody>
-    </Card>
-    <Card>
-      <CardBody>
-        <CardTitle className="text-center text-uppercase">
-          Advertisement
-        </CardTitle>
-        <img
-          src="http://via.placeholder.com/320x200"
-          alt="Advert"
-          style={{ width: "100%" }}
-        ></img>
-      </CardBody>
-    </Card>
-  </div>
-)
+				</CardBody>
+			</Card>
+		)}
+		<Card>
+			<CardBody>
+				<CardTitle>Recet Posts</CardTitle>
+				<StaticQuery
+					query={sidebarQuery}
+					render={(data) => (
+						<div>
+							{data.allMarkdownRemark.edges.map(({ node }) => (
+								<Card key={node.id}>
+									{!node.frontmatter.image ? (
+										''
+									) : (
+										<Link to={node.fields.slug}>
+											<Img
+												className='card-image-top'
+												fluid={node.frontmatter.image.childImageSharp.fluid}
+											/>
+										</Link>
+									)}
+									{/* <CardBody> */}
+									{/* <CardTitle> */}
+									<Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+									{/* </CardTitle> */}
+									{/* </CardBody> */}
+								</Card>
+							))}
+						</div>
+					)}
+				/>
+			</CardBody>
+		</Card>
+		<Card>
+			<CardBody>
+				<CardTitle className='text-center text-uppercase'>Advertisement</CardTitle>
+				<img src='http://via.placeholder.com/320x200' alt='Advert' style={{ width: '100%' }} />
+			</CardBody>
+		</Card>
+	</div>
+);
 
 const sidebarQuery = graphql`
-  query sidebarQuery {
-    allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC }
-      limit: 7
-    ) {
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            # image{
-            #   childImageSharp{
-            #     fluid(maxWidth: 300){
-            #       ...GatsbyImageSharpFluid
-            #     }
-            #   }
-            # }
-          }
-          fields {
-            slug
-          }
-        }
-      }
-    }
-  }
-`
+	query sidebarQuery {
+		allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: 7) {
+			edges {
+				node {
+					id
+					frontmatter {
+						title
+						# image{
+						#   childImageSharp{
+						#     fluid(maxWidth: 300){
+						#       ...GatsbyImageSharpFluid
+						#     }
+						#   }
+						# }
+					}
+					fields {
+						slug
+					}
+				}
+			}
+		}
+	}
+`;
 
-export default Sidebar
+export default Sidebar;
