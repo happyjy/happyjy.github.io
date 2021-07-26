78914915991258b03ba573a07d244ab2304e9a19
happyjy <okwoyjy@gmail.com>
2021-07-26 16:23:47 +0900

    change icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,7 +98,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/devto.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
